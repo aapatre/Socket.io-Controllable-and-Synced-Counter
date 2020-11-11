bfbc5b5bd94d54df5f7e4ba0126ea3505d89cb8c
Antariksh Patre <antarikshpatre@gmail.com>
2020-11-11 22:54:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get('/', (req,res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    
+    io.emit('what num', syncnum);
+    
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
